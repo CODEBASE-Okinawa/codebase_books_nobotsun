78b63d092c6a3fc1cd9c8bba7fb409933b158965
takanori-matsushita <tknr2899@gmail.com>
2023-02-13 15:40:23 +0000

    fix link

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -10,7 +10,7 @@ export const BookCard = () => {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button>詳細</Button>
+        <Button href={`/books/1`}>詳細</Button>
       </CardActions>
     </Card>
   )
