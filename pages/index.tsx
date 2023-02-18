08575f55198bdec283149fb1234c06867a86c0f7
Hitoshi-Noborikawa <dengchuanrenzhi@gmail.com>
2023-02-18 17:08:58 +0900

    tRPCを追加しました

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import dynamic from 'next/dynamic'
 import { Inter } from '@next/font/google'
 import { MainLayout } from '@/layouts/MainLayout'
 import { Session } from '@/components/Session'
+import { trpc } from '../utils/trpc'
+
+// const MainLayoutNoSSR = dynamic(() => import('@/layouts/MainLayout'), {
+//   ssr: false,
+// })
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <MainLayout>Main</MainLayout>
+  const hello = trpc.hello.useQuery({ text: 'tRPC' })
+  if (!hello.data) {
+    return <div>Loading...</div>
+  }
+  return <MainLayout>{hello.data.greeting}</MainLayout>
 }
