f28b23941e263a9d9471a120d93d250651fb5539
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2023-02-21 22:33:40 +0900

    🚧 BookCardコンポーネント修正に伴い、変更

--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -2,20 +2,18 @@ import { Grid } from '@mui/material'
 import { MainLayout } from '@/layouts/MainLayout'
 import { BookCard } from '@/components/BookCard'
 
+const Books_Infomation = [
+  { title: 'プログラミング言語Ruby', state: '貸し出し中', date: '2023-02-19', href: '/books/1' },
+  { title: 'プログラミング言語PHP', state: '予約中', date: '2023-02-19', href: '/books/2' },
+  { title: 'プログラミング言語JavaScript', state: '借りている', date: '2023-02-19', href: '/books/3' },
+  { title: 'プログラミング言語Python', state: '貸し出し可能', date: '2023-02-19', href: '/books/4' },
+]
+
 export default function Books() {
   return (
     <MainLayout>
-      <Grid item xs={12} md={4} lg={3}>
-        <BookCard />
-      </Grid>
-      <Grid item xs={12} md={4} lg={3}>
-        <BookCard />
-      </Grid>
-      <Grid item xs={12} md={4} lg={3}>
-        <BookCard />
-      </Grid>
-      <Grid item xs={12} md={4} lg={3}>
-        <BookCard />
+      <Grid container spacing={2}>
+        <BookCard {...{ books: Books_Infomation }} />
       </Grid>
     </MainLayout>
   )
