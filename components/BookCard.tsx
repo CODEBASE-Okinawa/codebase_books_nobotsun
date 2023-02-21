c8061658057f2d2d95fd33050766b49d22c7b00b
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2023-02-21 22:31:38 +0900

    🚧 BookCardコンポーネント変更

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,45 +1,49 @@
-import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box } from '@mui/material'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box, Grid } from '@mui/material'
 
-type BookInfo = {
-  title: string
-  state: string
-  date?: string
-  href: string
+export type Books = {
+  books: BookInfo[]
 }
 
-const Book_Infomation: BookInfo[] = [
-  { title: 'プログラミング言語Ruby', state: '貸し出し中', date: '2023-02-19', href: '/books/1' },
-  { title: 'プログラミング言語PHP', state: '予約中', date: '2023-02-19', href: '/books/2' },
-  { title: 'プログラミング言語JavaScript', state: '借りている', date: '2023-02-19', href: '/books/3' },
-  { title: 'プログラミング言語Python', state: '貸し出し可能', date: '2023-02-19', href: '/books/4' },
-]
+export type BookInfo = {
+  title?: string
+  state?: string
+  date?: string
+  href?: string
+}
 
-export const BookCard = () => {
+export const BookCard = (props: Books) => {
+  const { books } = props
   return (
     <>
-      {Book_Infomation.map((info, index) => (
-        <Card key={index}>
-          <CardMedia sx={{ height: 240 }} image="/images/no-image.jpg" title="no image" />
-          <CardContent>
-            <Typography gutterBottom variant="h6" component="h2">
-              {info.title}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <div style={{ width: '100%' }}>
-              <Box component="span" display="block" p={1} m={1}>
-                {info.state}
-              </Box>
+      <Grid container spacing={2}>
+        {books.map((book, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
+            <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+              <CardMedia sx={{ height: 240 }} image="/images/no-image.jpg" title="no image" />
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="h2">
+                  {book.title}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <div style={{ width: '100%' }}>
+                  <Box component="span" display="block" p={1} m={1}>
+                    {book.state}
+                  </Box>
 
-              <Box component="span" display="block" p={1} m={1}>
-                {info.date}
-              </Box>
+                  {book.state === '予約中' ? (
+                    <Box component="span" display="block" p={1} m={1}>
+                      {book.date}に予約済み
+                    </Box>
+                  ) : null}
 
-              <Button href={`/books/1`}>詳細</Button>
-            </div>
-          </CardActions>
-        </Card>
-      ))}
+                  <Button href={`/books/1`}>詳細</Button>
+                </div>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </>
   )
 }
