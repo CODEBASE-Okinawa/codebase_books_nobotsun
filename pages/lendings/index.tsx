da92d4d5e1f64d6402af7315fa43b9818d20f468
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2023-02-26 07:52:07 +0900

    ✨ 借りている本一覧ページ作成

---
+++ pages/lendings/index.tsx
@@ -0,0 +1,22 @@
+import { Grid } from '@mui/material'
+import { MainLayout } from '@/layouts/MainLayout'
+import { BookCard, BookInfo } from '@/components/BookCard'
+
+const Books_Lending_Info: BookInfo[] = [
+  { title: 'プログラミング言語Ruby', state: '借りている', date: '2023-02-19', href: '/books/1' },
+  { title: 'プログラミング言語PHP', state: '借りている', date: '2023-02-22', href: '/books/2' },
+  { title: 'プログラミング言語JavaScript', state: '借りている', date: '2023-02-21', href: '/books/3' },
+  { title: 'プログラミング言語Python', state: '借りている', date: '2023-02-25', href: '/books/4' },
+]
+
+const sortBooks = Books_Lending_Info.sort((a, b) => new Date(a.date!).getTime() - new Date(b.date!).getTime())
+
+export default function Books() {
+  return (
+    <MainLayout>
+      <Grid container spacing={2}>
+        <BookCard {...{ books: sortBooks }} />
+      </Grid>
+    </MainLayout>
+  )
+}
