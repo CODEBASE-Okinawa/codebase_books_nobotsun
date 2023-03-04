b3387972d79771b6c63b0b5701221eeddc96b3a3
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2023-03-04 17:44:17 +0900

    ✨ tRPCでデータを取得する実装追加

--- pages/reservations/index.tsx
+++ pages/reservations/index.tsx
@@ -1,21 +1,24 @@
 import { Grid } from '@mui/material'
 import { MainLayout } from '@/layouts/MainLayout'
 import { BookCard, BookInfo } from '@/components/BookCard'
-
-const Books_Reserve_Info: BookInfo[] = [
-  { title: 'プログラミング言語Ruby', status: '予約中', date: '2023-02-19', href: '/books/1', imageUrl: '/images/no-image.jpg' },
-  { title: 'プログラミング言語PHP', status: '予約中', date: '2023-02-22', href: '/books/2', imageUrl: '/images/no-image.jpg' },
-  { title: 'プログラミング言語JavaScript', status: '予約中', date: '2023-02-21', href: '/books/3', imageUrl: '/images/no-image.jpg' },
-  { title: 'プログラミング言語Python', status: '予約中', date: '2023-02-25', href: '/books/4', imageUrl: '/images/no-image.jpg' },
-]
-
-const sortBooks = Books_Reserve_Info.sort((a, b) => new Date(a.date!).getTime() - new Date(b.date!).getTime())
+import { trpc } from '@/utils/trpc'
 
 export default function Books() {
+  const getReservation = trpc.reservation.getEvent.useQuery({ bookId: 'clejnctos000cpiand94ldtdc' })
+
+  const reservationBooks: BookInfo[] = []
+  getReservation.data?.events.forEach((event) => {
+    reservationBooks.push({
+      title: getReservation.data?.title,
+      imageUrl: getReservation.data?.imageUrl,
+      status: event.status,
+      start: event.start,
+    })
+  })
   return (
     <MainLayout>
       <Grid container spacing={2}>
-        <BookCard {...{ books: sortBooks }} />
+        {getReservation.data && <BookCard books={reservationBooks} />}
       </Grid>
     </MainLayout>
   )
